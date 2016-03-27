e1e5d3eab5baa5f36a3bdd2a0cc58e4f73fd31b7
zltningx <zltningx@outlook.com>
2016-03-27 11:11:24 +0800

    Nix system etc shadow passwd cracker

--- Unix_etc_shadow_pass_geter/nix_passwd.py
+++ Unix_etc_shadow_pass_geter/nix_passwd.py
@@ -5,18 +5,15 @@ import crypt
 import sys
 from concurrent.futures import ThreadPoolExecutor
 
-def testpasswd(crypt_pass):
+def testpasswd(user,crypt_pass,word):
     salt = crypt_pass.split('$')[2]
     insalt = '${}${}$'.format('6',salt)
-    with open(sys.argv[2],'r') as dict_file:
-        for word in dict_file:
-            word = word.strip('\n')
-            crypt_word = crypt.crypt(word,insalt)
-            if crypt_pass == crypt_word:
-                print ("[+] Found Password: {}\n".format(word))
-                return
-    print ("[-]No Found {} ")
-    return
+    word = word.strip('\n')
+    crypt_word = crypt.crypt(word,insalt)
+    if crypt_pass == crypt_word:
+        print ("[*] Successful Cracked user : {} ".format(user))
+        print ("[+] Found Password: {}\n".format(word))
+        return
 
 def readpasswd():
     with open(sys.argv[1],'r') as passwd_file:
@@ -25,12 +22,16 @@ def readpasswd():
                 user = line.split(':')[0]
                 crypt_pass = line.split(':')[1].strip(' ')
                 if len(crypt_pass) > 16:
-                    print ("[*] Cracking Password For : {}".format(user))
                     with ThreadPoolExecutor(2) as Executor:
-                        Executor.submit(testpasswd,crypt_pass)
+                        with open(sys.argv[2],'r') as dict_file:
+                            for word in dict_file:
+                                Executor.submit(testpasswd,user,crypt_pass,word)
+
+    print ('[*] All Done!')
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print ("Usage:\n\t请准备好阴影文件和爆破字典\n\t如: ./nix_passwd.py password.txt dict.txt")
+        print ("Usage:\n\t请准备好阴影文件和爆破字典\n\t如: ./nix_passwd.py password.txt dict.txt ")
         sys.exit(0)
+    print ('[*] Runing... ...')
     readpasswd()
